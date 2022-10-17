b43499062ed68f1c7a7013f0b78be217e1183c70
Amit Bar-Gil <amit217@yandex.com>
2022-10-17 10:31:09 +0300

    style: social links

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,36 +7,38 @@ function Menu(props: { showMenu: boolean }) {
     return (
         <div className={`menu ${props.showMenu && `menu_show`}`}>
             <nav className='menu__nav'>
-                <p className='menu__item'>hello</p>
-                <p className='menu__item'>about me</p>
-                <p className='menu__item'>projects</p>
-                <p className='menu__item'>contact</p>
+                <p className='menu__item'>/hello</p>
+                <p className='menu__item'>/about me</p>
+                <p className='menu__item'>/projects</p>
+                <p className='menu__item'>/contact</p>
             </nav>
             <div className='menu__footer'>
                 <p className='menu__footer-caption'>find me in: </p>
                 <div className='menu__footer-links'>
-                    <a
-                        href='https://www.linkedin.com/in/amitbg/'
-                        target={"_blank"}
-                        rel='noreferrer'
-                        className='menu__link'>
-                        <img
-                            className='menu__social menu__social_linkedin'
-                            src={linkedinIcon}
-                            alt='linkedin icon'
-                        />
-                    </a>
-                    <a
-                        href='https://github.com/AmitGit217'
-                        target={"_blank"}
-                        rel='noreferrer'
-                        className='menu__link'>
-                        <img
-                            className='menu__social'
-                            src={githubIcon}
-                            alt='github icon'
-                        />
-                    </a>
+                    <div className='menu__social'>
+                        <a
+                            href='https://www.linkedin.com/in/amitbg/'
+                            target={"_blank"}
+                            rel='noreferrer'>
+                            <img
+                                src={linkedinIcon}
+                                alt='linkedin icon'
+                                className='menu__footer-image'
+                            />
+                        </a>
+                    </div>
+                    <div className='menu__social'>
+                        <a
+                            href='https://github.com/AmitGit217'
+                            target={"_blank"}
+                            rel='noreferrer'>
+                            <img
+                                src={githubIcon}
+                                alt='github icon'
+                                className='menu__footer-image'
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>

e7b7748a47e06c8a48729f28e64c4637407bc980
Amit Bar-Gil <amit217@yandex.com>
2022-10-19 11:15:11 +0300

    style: personal info computer screen

--- src/components/AboutComp.tsx
+++ src/components/AboutComp.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import SideBar from "./SideBar";
+import { CodeBlock, dracula } from "react-code-blocks";
+import "../blocks/about-comp.css";
 
 export default function AboutComp() {
     const [showData, setData] = useState("personal-info");
@@ -7,11 +9,30 @@ export default function AboutComp() {
         setData(str);
     };
     return (
-        <section>
+        <section className='about-comp'>
             <SideBar
                 nav={["personal-info", "professional-info"]}
                 compToShow={handleDataDisplay}
+                isActive={showData}
             />
+            {showData === "personal-info" && (
+                <div className='about-comp__code'>
+                    <CodeBlock
+                        text={`/*
+* Hello there. My name is Amit Bar-Gil and
+* I'm 22 years old and currently live in
+* Afula (North Israel). I love to learn
+* new things, challenge myself and
+* collaborate with others on projects and
+* interesting ideas. Some of my hobbies:
+* music, sport, and chess.
+*/`}
+                        language={"js"}
+                        showLineNumbers={true}
+                        theme={dracula}
+                    />
+                </div>
+            )}
         </section>
     );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "../blocks/sidebar.css";
 
-export default function SideBar(props: { nav: string[]; compToShow: any }) {
+export default function SideBar(props: {
+    nav: string[];
+    compToShow: any;
+    isActive: string;
+}) {
     return (
         <div className='sidebar'>
             {props.nav.map((nav) => {
                 return (
                     <p
-                        className='sidebar__nav'
+                        className={`sidebar__nav  ${
+                            props.isActive === nav && "sidebar__nav_active"
+                        }`}
                         onClick={() => props.compToShow(nav)}>
                         {nav}
                     </p>
